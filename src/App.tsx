b9e476626ffb0cece30d3ee6c888e69b3fa8a17f
Robert Puscasu <robert.puscasu@tradeshift.com>
2020-03-14 16:03:58 +0200

    adding topbar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import './App.css';
 import Main from './layouts/Main/Main';
 import { Switch, Route } from 'react-router-dom'
 import Shop from './pages/Shop/Shop';
+import TopBar from './layouts/Main/Topbar/Topbar'
 
 const App: React.FC = () => {
   return (
     <div>
+      <TopBar />
       <Switch>
         <Route  exact path='/' component={Main} />
         <Route  path='/shop' component={Shop} />
