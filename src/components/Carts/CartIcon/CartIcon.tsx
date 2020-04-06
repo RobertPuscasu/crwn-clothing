dd5923188928a4103cb0ced7d4d5603763af972a
Robert Puscasu <robert.puscasu@tradeshift.com>
2020-04-06 11:19:04 +0300

    Memoized selectors

--- src/components/Carts/CartIcon/CartIcon.tsx
+++ src/components/Carts/CartIcon/CartIcon.tsx
@@ -2,19 +2,27 @@ import * as React from 'react';
 
 import { ReactComponent as ShoppingIcon } from '../../../assets/shopping-bag.svg';
 import './style.scss';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { cartAction } from 'src/store/cart/cart.actions';
+import { RootState } from 'typesafe-actions';
+import { createSelector } from 'reselect';
+import { IShopItem } from '../../../interfaces/models/shop-item.model';
 
-const CartIcon: React.FC = () => {
+const selectItemCount = createSelector(
+  (state: RootState) => (state.cart.addCart.cartItems) ,
+  (cartItems: IShopItem[]) => cartItems.reduce((acc, cartItem) => acc + cartItem.quantity,0)
+);
 
-  const dispatch  = useDispatch();
+const CartIcon: React.FC = React.memo(() => {
+  const dispatch = useDispatch();
+  const itemCount = useSelector(selectItemCount);
 
   return (
     <div className="cart-icon" onClick={e => dispatch(cartAction())}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
-};
+});
 
 export default CartIcon;
