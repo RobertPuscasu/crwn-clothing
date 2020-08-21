6b77e35b2caf9548abb1c053b7e6f60ae92f56b4
Robert Puscasu <robert.puscasu@tradeshift.com>
2020-08-21 10:46:41 +0300

    Added logger only on dev

--- src/store/store.ts
+++ src/store/store.ts
@@ -14,8 +14,11 @@ const composeEnhancers =
 const initialState = {}
 
 
-const middlewares = [logger, epicMiddleware]
+const middlewares = [ epicMiddleware]
 
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 export const history = createBrowserHistory();
 
 export const store: Store = createStore(
